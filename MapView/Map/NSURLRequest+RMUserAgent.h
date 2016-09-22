19caa400892984366f63d47896c610ca7ff3aeb1
Natalia_Osiecka <osiecka.n@gmail.com>
2016-09-22 12:20:11 +0100

    Added documentation to the category methods.

--- MapView/Map/NSURLRequest+RMUserAgent.h
+++ MapView/Map/NSURLRequest+RMUserAgent.h
@@ -10,7 +10,24 @@
 
 @interface NSURLRequest (RMUserAgent)
 
+/**
+ Creates and returns an initialized URL request with specified values. Appends information about user agent to the header.
+
+ @param url The URL for the new request.
+
+ @return The newly created URL request.
+ */
 + (instancetype)rm_requestWithHeaderForURL:(NSURL *)url;
+
+/**
+ Creates and returns an initialized URL request with specified values. Appends information about user agent to the header.
+
+ @param url             The URL for the new request.
+ @param cachePolicy     The cache policy for the new request.
+ @param timeoutInterval The timeout interval for the new request, in seconds.
+
+ @return The newly created URL request.
+ */
 + (instancetype)rm_requestWithHeaderForURL:(NSURL *)url cachePolicy:(NSURLRequestCachePolicy)cachePolicy timeoutInterval:(NSTimeInterval)timeoutInterval;
 
 @end
--- MapView/Map/NSURLSession+RMUserAgent.h
+++ MapView/Map/NSURLSession+RMUserAgent.h
@@ -10,7 +10,28 @@
 
 @interface NSURLSession (RMUserAgent)
 
+/**
+ Stops the thread to perform a synchronous request to the server.
+ @warning Not intended to use on the main thread.
+
+ @param request An NSURLRequest object that provides the URL, cache policy, request type, body data or body stream, and so on.
+ @param error   An error object that indicates why the request failed, or nil if the request was successful.
+
+ @return The data returned by the server.
+ */
 + (NSData *)rm_fetchDataSynchronouslyWithRequest:(NSURLRequest *)request error:(NSError **)error;
+
+
+/**
+ Stops the thread to perform a synchronous request to the server.
+ @warning Not intended to use on the main thread.
+
+ @param request  An NSURLRequest object that provides the URL, cache policy, request type, body data or body stream, and so on.
+ @param error    An error object that indicates why the request failed, or nil if the request was successful.
+ @param response An object that provides response metadata, such as HTTP headers and status code. If you are making an HTTP or HTTPS request, the returned object is actually an NSHTTPURLResponse object.
+
+ @return The data returned by the server.
+ */
 + (NSData *)rm_fetchDataSynchronouslyWithRequest:(NSURLRequest *)request error:(NSError **)error response:(NSURLResponse **)response;
 
 @end
